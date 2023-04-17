f4fe7f45119c7dab32af58e8e4e467b8fd5ff749
SzarneckiP <siarniuk@gmail.com>
2023-04-17 03:37:22 +0200

    fix bugs with Sanity

--- sanity_tiktung/sanity.config.ts
+++ sanity_tiktung/sanity.config.ts
@@ -1,14 +1,18 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+// import { defineConfig } from 'sanity'
+import { defineConfig } from 'sanity/lib/exports'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+
+const projectId: any = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset: any = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export default defineConfig({
   name: 'default',
   title: 'tiktung',
 
-  projectId: 'gqqx8jm3',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
 
   plugins: [deskTool(), visionTool()],
 
